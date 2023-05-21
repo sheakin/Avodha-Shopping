a5f13a7c9c186095d03168339636ccc496f0f63a
Sheakin <sheakin92@gmail.com>
2023-05-22 01:26:00 +0530

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
-# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        # Retrieve form data
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email taken')
+                return redirect('index')
+            else:
+    # Create a new user
+                 user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email,password=password)
+                 user.save();
+                 print("user created")
+                 messages.success(request, 'Registration successful. Please log in.')
+                 return redirect('login')  # Redirect to the login page
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username is not None and password is not None:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('/')
+            else:
+                messages.info(request, 'Invalid credentials')
+                return redirect('login')
+        else:
+            messages.info(request, 'Please provide username and password')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
--- avodhashop/urls.py
+++ avodhashop/urls.py
@@ -19,10 +19,14 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('cart/',include('cart.urls')),
-    path('',include('shop.urls')),
-  
+    path('accounts/',include('accounts.urls')),
+    path('',include('shop.urls')), 
+
+
+
 ]
 
 urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
